c10e8e8cb8a7805c0b3ce609992d19879d257095
soeren <56670304+soerenschneider@users.noreply.github.com>
2021-12-31 13:16:47 +0100

    Add script to easily delete state for workspace

---
+++ scripts/delete-all-resources-from-state.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "You are about to delete the current resources from state for workspace $(terraform workspace show):"
+echo "------------------------------------------------------------"
+
+terraform state list
+
+echo "------------------------------------------------------------"
+read -p "Are you sure? " -n 1 -r
+echo ""
+if [[ ${REPLY} =~ ^[Yy]$ ]]
+then
+    echo "Proceeding"
+    #terraform state list | cut -f 1 -d '[' | xargs -L 1 terraform state rm
+else
+    echo "OK, bye"
+fi
+
